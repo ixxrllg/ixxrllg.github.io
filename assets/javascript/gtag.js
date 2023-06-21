b0f676a7e076c7c6a904811d3774b71ed637641f
ixxrllg <kupeczkapriv66@gmail.com>
2023-06-21 12:13:47 +0200

    dziala chyba nie

---
+++ assets/javascript/gtag.js
@@ -0,0 +1,10 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-39EZKESN39"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-39EZKESN39');
+  gtag('event', 'page_view', { 'send_to': 'G-39EZKESN39' });
+</script>
