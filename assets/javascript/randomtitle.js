d85b1e48cbbc41cc50723130608c43dc4ce32b45
ixxrllg <kupeczkapriv66@gmail.com>
2023-07-05 12:01:39 +0200

    nowe

---
+++ assets/javascript/randomtitle.js
@@ -0,0 +1,5 @@
+// randomowy tytuł strony
+
+  var titles = ['Książe z Kopenhagi', 'Mam w głowie xanax', 'Z', 'Czy to sen?', 'W sercu tylko ona', 'Kocham zosie']
+  var title = titles[Math.floor(Math.random() * titles.length)]
+  document.title = title
